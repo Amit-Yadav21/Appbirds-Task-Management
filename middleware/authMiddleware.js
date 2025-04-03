93218a8a188e5fa0ea4a13620d82f276d3d7d76b
Amit-Yadav21 <amit21@navgurukul.org>
2025-04-03 11:40:15 +0530

    same correction

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,3 @@
-// import jwt from "jsonwebtoken"; // Import JWT for token verification
-
-// // Authentication middleware
-// const authMiddleware = (req, res, next) => {
-//     const token = req.header("Authorization"); // Get token from headers
-//     if (!token) return res.status(401).json({ message: "Access denied" }); // Check if token exists
-
-//     try {
-//         const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET); // Verify token
-//         req.user = decoded; // Attach user info to request
-//         next(); // Proceed to next middleware
-//     } catch (error) {
-//         res.status(400).json({ message: "Invalid token" }); // Handle invalid token
-//     }
-// };
-
-// export default authMiddleware;
-
-
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
