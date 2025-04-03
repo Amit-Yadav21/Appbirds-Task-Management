c2a5d27ed9a1df0a8379da220821fd3e67f2c75c
Amit-Yadav21 <amit21@navgurukul.org>
2025-04-03 08:58:09 +0530

    Some correction in DB

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ import mongoose from "mongoose";
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // Prevent deprecation warnings
-            useUnifiedTopology: true, // Optimize connection handling
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected"); // Success message
     } catch (error) {
         console.error("MongoDB Connection Error:", error); // Log error
